9fecdf45983aa5cdb5b887024e5560dac67b052a
Bonam Phani <phani.bonam45@gmail.com>
2019-10-01 20:01:37 +0530

    added numbers

--- l.java
+++ l.java
@@ -2,7 +2,12 @@ public class l
 {
 	public static void main (String args[])
 	{
+	int i;
+	for (i=0;i<=6;i++);
+	{
 	System.out.println("sriganesha");
 	System.out.println("\n phani");
+	System.out.println("numbers", +i);
 	}
+}
 }
