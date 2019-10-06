7e68bed65401347b7606e5058ed56101266e4524
Bonam Phani <phani.bonam45@gmail.com>
2019-10-06 16:27:49 +0530

    prints no's upto 9

--- l.java
+++ l.java
@@ -3,7 +3,7 @@ public class l
 	public static void main (String args[])
 	{
 	int i;
-	for (i=0;i<=6;i++);
+	for (i=0;i<=9;i++);
 	{
 	System.out.println("sriganesha");
 	System.out.println("\n phani");
